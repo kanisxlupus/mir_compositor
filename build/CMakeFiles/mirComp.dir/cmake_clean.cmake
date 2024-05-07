118dce462947340b82a7f3bcdaacbefd57dc42bd
Sam Jones <srjonesld@gmail.com>
2024-05-06 18:38:55 -0700

    compiled hello world program, wrote bash script for easier execution

---
+++ build/CMakeFiles/mirComp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mirComp.dir/main.cpp.o"
+  "CMakeFiles/mirComp.dir/main.cpp.o.d"
+  "mirComp"
+  "mirComp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mirComp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
